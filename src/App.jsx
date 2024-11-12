18c4922632cc1eb4815ea9d736c79114aee09bbf
shubham <shukhinchi9829che@gmail.com>
2024-11-12 18:51:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import Navigation from './compnents/Navigation'
+import './App.css'
+import HeroSection from './compnents/HeroSection'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navigation/>
+        <HeroSection/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
